3fd3e3f935cf56738227d74b7b8627d6f1c36564
João Campos <joaog.taborda.campos@gmail.com>
2022-06-14 02:05:10 -0300

    Criando tabela para conter os livros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LivrosController } from './livros_controller';
 import { LivrosService } from './livros_services';
+import { Livro } from './livro_model';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'admin',
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro])
+  ],
   controllers: [AppController, LivrosController],
   providers: [AppService, LivrosService],
 })
--- src/livros_services.ts
+++ src/livros_services.ts
@@ -5,9 +5,9 @@ import { Livro } from "./livro_model";
 @Injectable()
 export class LivrosService {
   livros: Livro[] = [
-    new Livro("LIV01", "Livro TDD e BDD na prática", 29.00),
-    new Livro("LIV02", "Iniciando com flutter", 39.00),
-    new Livro("LIV03", "inteligência artificial como serviço", 29.00)
+    // new Livro("LIV01", "Livro TDD e BDD na prática", 29.00),
+    // new Livro("LIV02", "Iniciando com flutter", 39.00),
+    // new Livro("LIV03", "inteligência artificial como serviço", 29.00)
   ];
 
   obterTodos(): Livro[] {
